4a2cd2fdcfde67246cdc899f2889fa21a0d8ed8c
Várkonyi István <kbvconsulting@gmail.com>
2026-03-17 14:30:09 +0100

    Base SQL purged

--- docs/sql/PGSQL/files_db/files.sql
+++ docs/sql/PGSQL/files_db/files.sql
@@ -30,9 +30,3 @@ CREATE TABLE "public"."files" (
     PRIMARY KEY ("id")
 );
 
-INSERT INTO "public"."files" ("id", "name", "mime_type", "size", "description", "tags", "uploaded", "project", "folder", "file_path", "uploaded_by") VALUES
-(1, '120Autocut.jpg', 'image/jpeg', 180075, '', '{}', '2026-03-13', '', 'labels', 'labels/1c911e0c-0d34-493e-9c3b-cdc236b6d7e4.jpg', 'admin'),
-(2, '20240823_megrendeles_gyartasiutasitasok 2.xlsx', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 15157, '', '{order,production}', '2026-03-13', '', 'riport', 'riport/68f6316c-b477-4858-934a-928e8f209ca5.xlsx', 'admin'),
-(3, 'auth-ui-demo.mp4', 'video/mp4', 19333862, '', '{video,signin}', '2026-03-13', '', 'videos', 'videos/9772dcd2-1942-416a-834c-0be349b03039.mp4', 'admin'),
-(4, 'sap-sync-ui-demo.mp4', 'video/mp4', 7908427, '', '{video,signin}', '2026-03-13', '', 'videos', 'videos/932a7101-a632-4422-8729-b38216eecedf.mp4', 'admin'),
-(5, '7692924-hd_1920_1080_25fps.mp4', 'video/mp4', 7999528, '', '{video,sales}', '2026-03-13', '', 'videos', 'videos/14d4cc3f-8d1c-45b9-a82f-f390b1af9dcb.mp4', 'admin');
